ceef37fb9ed6a118486d6cb5fc435a6a5e11a2c8
Marloto <Marloto@users.noreply.github.com>
2023-09-29 09:58:10 +0200

    Update server.py

--- 22-client-server/server.py
+++ 22-client-server/server.py
@@ -20,23 +20,32 @@ class Address:
 
 app = Flask(__name__)
 
+# localhost -> 127.0.0.1
+# http://localhost:5000/test
 @app.route('/test')
 def test():
     return "Hello, World!"
 
+# http://localhost:5000/address
+# HTTP definiert mehrere "Methoden" GET / POST
+# zur vereinfachung ist das folgende nur ein "GET" ohne Daten, daher wird ein Dummy Eintrag erzeugt
 @app.route('/address')
 def address():
     address = Address("Ich", "Dort 1", "12345", "Irgendwoe")
     result = address.to_json()
     return result
+# Ergebnis wäre etwas der Form: {"name": "Ich", "street": "Dort 1", ...}
 
 
+# http://localhost:5000/addresses
+# Ebenfalls GET
 @app.route('/addresses')
 def addresses():
     address1 = Address("Ich", "Dort 1", "12345", "Irgendwoe")
     address2 = Address("Du", "Sonstwo 2", "99999", "Anderswo")
     result = json.dumps([address1.__dict__, address2.__dict__])
     return result
+# Ergebnis wäre etwas der Form: [{"name": "Ich", "street": "Dort 1", ...}, {"name": "Du", "street": "Sonstwo 2", ...}]
 
 if __name__ == '__main__':
    app.run(port=5000, debug=True)
